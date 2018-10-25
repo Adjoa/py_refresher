1c4bf6b1120ff5b09f791af6ea02b826cca1d6dc
adjoadarien <adjoa.darien@gmail.com>
2018-10-25 09:43:26 -0400

    reviewed if stmts, booleans, and comments

--- refresh.py
+++ refresh.py
@@ -7,3 +7,24 @@ age = 2000
 
 print("Hello, world! My name is {}, and I am {} years old.".format(name, age))
 
+# If Statements and Comments
+isHot = True
+
+if isHot:
+    print("Hace calor!")
+else:
+    print("Hace frio.")
+
+if name == "Julie":
+    print("The name is Julie.")
+
+if age > 2000:
+    print("Age is greater than 2000")
+
+"""
+This is a 
+multi-line 
+comment!
+"""
+
+
