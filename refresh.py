f2e0fbf23c7aa1bf9273621a24cc26b72ce3b665
adjoadarien <adjoa.darien@gmail.com>
2018-10-25 09:26:52 -0400

    reviewed vars, strings, ints, and print

---
+++ refresh.py
@@ -0,0 +1,9 @@
+# Hello, world!
+print("Hello, world!")
+
+# Variables, Strings, Ints, and Print
+name = "Adjoa"
+age = 2000
+
+print("Hello, world! My name is {}, and I am {} years old.".format(name, age))
+
